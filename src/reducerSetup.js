811e87be32d176cfebc4a8588271770b70858a91
Max Moeschinger <m.moeschinger@sportradar.com>
2017-07-11 13:08:51 +0200

    CHANGE: component to setup reducer

---
+++ src/reducerSetup.js
@@ -0,0 +1,13 @@
+import reducer from './reducer';
+
+export default (reducers) => {
+    let stores = {};
+    reducers.forEach(reducerInfo => {
+        if (typeof reducerInfo !== 'object') {
+            stores[reducerInfo] = reducer(reducerInfo, '/' + reducerInfo);
+        } else {
+            stores[reducerInfo.storeName] = reducer(reducerInfo.prefix, reducerInfo.baseUrl);
+        }
+    });
+    return stores;
+};
