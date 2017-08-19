e83d420c7af0a58a56932c3639f6a920a6b0c3c5
Max Moeschinger <m.moeschinger@gmail.com>
2017-08-19 16:24:16 +0200

    fix endless fetching loop

--- src/selectors/shouldLoadSingle.js
+++ src/selectors/shouldLoadSingle.js
@@ -5,5 +5,5 @@ export default (state, id) => {
     if (!state.meta[id]) {
         return true;
     }
-    return !(state.meta[id].isLoading || state.meta[id].loadedAt);
+    return !(state.meta[id].isLoading || state.meta[id].loadedAt || state.meta[id].error);
 }
