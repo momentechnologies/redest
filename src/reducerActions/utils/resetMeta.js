df0a382b9a555eb9bef74cda6b64b3bd63e306d2
Max Moeschinger <m.moeschinger@sportradar.com>
2017-09-22 14:28:21 +0200

    make eslint happy

--- src/reducerActions/utils/resetMeta.js
+++ src/reducerActions/utils/resetMeta.js
@@ -1,9 +1,7 @@
-export default (meta) => Object.keys(meta).reduce((acc, getKey) => {
-    return {
-        ...acc,
-        [getKey]: {
-            ...meta[getKey],
-            loadedAt: false
-        }
-    };
-}, {});
+export default (meta) => Object.keys(meta).reduce((acc, getKey) => ({
+    ...acc,
+    [getKey]: {
+        ...meta[getKey],
+        loadedAt: false
+    }
+}), {});
