f5822a9905e986cb595c7cc14861e5b7a9773b6b
Max Moeschinger <m.moeschinger@gmail.com>
2017-11-06 19:13:05 +0100

    bugfix

--- src/selectors/selectRequestData.js
+++ src/selectors/selectRequestData.js
@@ -2,8 +2,10 @@ import { isSingle, isAll, isMultiple } from './selectMetaKey';
 
 export default (info = {}) => {
     if (isSingle(info.filter)) return null;
-    if (!info.pagination && isAll(info.filter)) {
-        return null;
+    if (isAll(info.filter)) {
+        return {
+            ...info.pagination,
+        };
     }
     return {
         ...info.filter,
